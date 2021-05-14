cd52098df33bdf2fce37c6bb56b46d44f1185abc
AJRego <rego.andre99@gmail.com>
2021-05-14 17:17:44 -0500

    Crear clase database factory encargada de devolver un database

--- src/main/java/com/ajrego/inventario/model/database/DatabaseFactory.java
+++ src/main/java/com/ajrego/inventario/model/database/DatabaseFactory.java
@@ -2,4 +2,10 @@ package com.ajrego.inventario.model.database;
 
 public class DatabaseFactory {
 
+    public static Database getDatabase(String name) {
+        if (name.equals("sqlite")) {
+            return new DatabaseSQLite();
+        }
+        return null;
+    }
 }
