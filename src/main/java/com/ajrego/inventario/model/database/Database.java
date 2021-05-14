08bd18cd24cd4316054f00890a07c9937148f4c8
AJRego <rego.andre99@gmail.com>
2021-05-14 17:12:54 -0500

    Interfaz con los metodos de una base de datos

--- src/main/java/com/ajrego/inventario/model/database/Database.java
+++ src/main/java/com/ajrego/inventario/model/database/Database.java
@@ -1,5 +1,10 @@
 package com.ajrego.inventario.model.database;
 
+import java.sql.Connection;
+
 public interface Database {
 
+    public Connection connect(String database);
+
+    public void close(Connection connection);
 }
