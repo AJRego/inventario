b9a8a998769680bd25275714433892932a8d20d0
AJRego <rego.andre99@gmail.com>
2021-05-14 17:15:08 -0500

    Implementar la interfaz database en sqlite

--- src/main/java/com/ajrego/inventario/model/database/DatabaseSQLite.java
+++ src/main/java/com/ajrego/inventario/model/database/DatabaseSQLite.java
@@ -1,5 +1,32 @@
 package com.ajrego.inventario.model.database;
 
-public class DatabaseSQLite {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseSQLite implements Database {
+
+    private Connection connection;
+
+    @Override
+    public Connection connect(String database) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + database);
+        } catch (SQLException se) {
+            Logger.getLogger(DatabaseSQLite.class.getName()).log(Level.SEVERE, null, se);
+        }
+        return connection;
+    }
+
+    @Override
+    public void close(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException se) {
+            Logger.getLogger(DatabaseSQLite.class.getName()).log(Level.SEVERE, null, se);
+        }
+    }
 
 }
