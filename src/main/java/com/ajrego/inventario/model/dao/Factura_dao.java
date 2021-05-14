8abda44046447ea6bbbc800ac982a58265113827
Sergio Gonzalez <gunner0419@gmail.com>
2021-05-14 18:16:09 -0500

    Agragamos a los archivos del package DAO las variables PreparedStatement y ResultSet, ademas de instanciar la clase connection!

--- src/main/java/com/ajrego/inventario/model/dao/Factura_dao.java
+++ src/main/java/com/ajrego/inventario/model/dao/Factura_dao.java
@@ -1,6 +1,15 @@
 
 package com.ajrego.inventario.model.dao;
 
+import com.ajrego.inventario.model.database.DatabaseSQLite;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Factura_dao {
+    
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
+    DatabaseSQLite connection = new DatabaseSQLite();
     
 }
--- src/main/java/com/ajrego/inventario/model/dao/Proveedor_dao.java
+++ src/main/java/com/ajrego/inventario/model/dao/Proveedor_dao.java
@@ -1,7 +1,16 @@
 
 package com.ajrego.inventario.model.dao;
 
+import com.ajrego.inventario.model.database.DatabaseSQLite;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class Proveedor_dao {
+    
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
+    DatabaseSQLite connection = new DatabaseSQLite();
     
 }
